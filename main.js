3d7e96d290be62c23ec4e7a56b235ccad9ca2e87
Diego Iberri <creativoo@gmail.com>
2023-04-04 00:21:12 -0700

    guardando dogs en favoritos

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const API_URL_FAVORITES = "https://api.thedogapi.com/v1/favourites?api_key=live_
 const spanError = document.getElementById('error');
 
 async function loadRandomDogs() {   // Cargando la API de perros con resultados "Random"
-    const res = await fetch(API_URL_RANDOM);
+    const res = await fetch(API_URL_RANDOM); 
     const data = await res.json();
     console.log('Random');
     console.log(data);
@@ -15,8 +15,13 @@ async function loadRandomDogs() {   // Cargando la API de perros con resultados 
     } else {
         const img1 = document.getElementById('img1');
         const img2 = document.getElementById('img2');
+        const btn1 = document.getElementById('btn1');
+        const btn2 = document.getElementById('btn2');
         img1.src = data[0].url;
         img2.src = data[1].url;
+
+        btn1.onclick = () => saveFavoriteDogs(data[0].id);
+        btn2.onclick = () => saveFavoriteDogs(data[1].id);
     }
     };
 
@@ -28,17 +33,33 @@ async function loadRandomDogs() {   // Cargando la API de perros con resultados 
 
         if (res.status !== 200) { // Agregando un condicional que lance un error si el Codigo HTTP es mayor a 200
             spanError.innerHTML = 'Hubo un error:' + rest.status + data.message;
-    };
+        } else {
+            data.forEach(dog => {
+                const section = document.getElementById('favoriteDogs')
+                const article = document.createElement('article');
+                const img = document.createElement('img');
+                const btn = document.createElement('button');
+                const btnText = document.createTextNode('Sacar de Favoritos');
+
+                img.src = dog.image.url;
+                img.width = 250;
+                btn.appendChild(btnText);
+                article.appendChild(img);
+                article.appendChild(btn);
+                section.appendChild(article);
+
+            });
+        };
 };
 
-async function saveFavoriteDogs() {
+async function saveFavoriteDogs(id) {
     const res = await fetch (API_URL_FAVORITES, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            image_id:"uISezUGDV"
+            image_id:id
         }),
     });
     const data = await res.json();
